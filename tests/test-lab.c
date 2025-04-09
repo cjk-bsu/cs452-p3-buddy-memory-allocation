1914cffc857408603823a106ec787c42747852d9
Carson <123137691+cjk-bsu@users.noreply.github.com>
2025-04-09 04:59:10 +0000

    Added additional test cases to expand code coverage

--- tests/test-lab.c
+++ tests/test-lab.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #ifdef __APPLE__
 #include <sys/errno.h>
 #else
@@ -10,14 +11,15 @@
 #include "../src/lab.h"
 
 
-void setUp(void) {
+void setUp(void) 
+{
   // set stuff up here
 }
 
-void tearDown(void) {
+void tearDown(void) 
+{
   // clean stuff up here
 }
-
 
 
 /**
@@ -85,7 +87,7 @@ void test_buddy_malloc_one_byte(void)
  */
 void test_buddy_malloc_one_large(void)
 {
-  fprintf(stderr, "->Testing size that will consume entire memory pool\n");
+  fprintf(stderr, "->Testing size will consume entire memory pool\n");
   struct buddy_pool pool;
   size_t bytes = UINT64_C(1) << MIN_K;
   buddy_init(&pool, bytes);
@@ -120,7 +122,7 @@ void test_buddy_malloc_one_large(void)
  */
 void test_buddy_init(void)
 {
-  fprintf(stderr, "->Testing buddy init\n");
+  fprintf(stderr, "->Testing buddy_init\n");
   //Loop through all kval MIN_k-DEFAULT_K and make sure we get the correct amount allocated.
   //We will check all the pointer offsets to ensure the pool is all configured correctly
   for (size_t i = MIN_K; i <= DEFAULT_K; i++)
@@ -133,6 +135,188 @@ void test_buddy_init(void)
     }
 }
 
+void test_btok(void)
+{
+  fprintf(stderr, "->Testing btok ddge cases\n");
+  TEST_ASSERT_EQUAL(SMALLEST_K, btok(1));
+  TEST_ASSERT_EQUAL(11, btok(1025));
+  TEST_ASSERT_EQUAL(20, btok(1 << 20));
+  TEST_ASSERT_EQUAL(21, btok((1 << 20) + 1));
+}
+
+void test_multiple_allocs_and_frees(void)
+{
+  fprintf(stderr, "->Testing multiple allocs & frees w/ merges\n");
+  struct buddy_pool pool;
+  buddy_init(&pool, UINT64_C(1) << (SMALLEST_K + 2)); // enough space for 4 small blocks
+
+  void *a = buddy_malloc(&pool, 8);
+  void *b = buddy_malloc(&pool, 8);
+  void *c = buddy_malloc(&pool, 8);
+  void *d = buddy_malloc(&pool, 8);
+
+  TEST_ASSERT_NOT_NULL(a);
+  TEST_ASSERT_NOT_NULL(b);
+  TEST_ASSERT_NOT_NULL(c);
+  TEST_ASSERT_NOT_NULL(d);
+
+  // Free in different order to check merge logic
+  buddy_free(&pool, b);
+  buddy_free(&pool, a);
+  buddy_free(&pool, d);
+  buddy_free(&pool, c);
+
+  check_buddy_pool_full(&pool);
+  buddy_destroy(&pool);
+}
+
+void test_null_and_zero_cases(void)
+{
+  fprintf(stderr, "->Testing NULL and zero-size inputs\n");
+
+  void *mem = buddy_malloc(NULL, 100);
+  TEST_ASSERT_NULL(mem);
+
+  struct buddy_pool pool;
+  buddy_init(&pool, UINT64_C(1) << MIN_K);
+
+  mem = buddy_malloc(&pool, 0);
+  TEST_ASSERT_NULL(mem);
+
+  buddy_free(&pool, NULL);
+
+  void *r = buddy_realloc(&pool, NULL, 128);
+  TEST_ASSERT_NOT_NULL(r);
+  buddy_free(&pool, r);
+
+  r = buddy_realloc(&pool, r, 0);
+  TEST_ASSERT_NULL(r);
+
+  buddy_destroy(&pool);
+}
+
+void test_realloc_resize_up_and_down(void)
+{
+  fprintf(stderr, "->Testing realloc resizing\n");
+  struct buddy_pool pool;
+  buddy_init(&pool, UINT64_C(1) << MIN_K);
+
+  void *ptr = buddy_malloc(&pool, 16);
+  TEST_ASSERT_NOT_NULL(ptr);
+
+  void *larger = buddy_realloc(&pool, ptr, 1024);
+  TEST_ASSERT_NOT_NULL(larger);
+
+  void *smaller = buddy_realloc(&pool, larger, 8);
+  TEST_ASSERT_NOT_NULL(smaller);
+
+  buddy_free(&pool, smaller);
+  check_buddy_pool_full(&pool);
+  buddy_destroy(&pool);
+}
+
+void test_randomized_alloc_free(void)
+{
+  fprintf(stderr, "->Testing randomized allocs & frees\n");
+  struct buddy_pool pool;
+  buddy_init(&pool, UINT64_C(1) << (SMALLEST_K + 6)); // 4KB pool
+
+  const int num_ptrs = 128;
+  void *ptrs[num_ptrs];
+  memset(ptrs, 0, sizeof(ptrs));
+
+  for (int i = 0; i < num_ptrs; ++i) {
+    size_t size = rand() % 128 + 1;
+    ptrs[i] = buddy_malloc(&pool, size);
+    if (ptrs[i]) {
+      struct avail *block = ((struct avail *)ptrs[i]) - 1;
+      size_t block_size = (1UL << block->kval);
+      size_t usable_size = block_size - sizeof(struct avail);
+      memset(ptrs[i], (int)i, usable_size);
+    }
+  }
+
+  for (int i = 0; i < num_ptrs; ++i) {
+    int idx = rand() % num_ptrs;
+    if (ptrs[idx]) {
+      buddy_free(&pool, ptrs[idx]);
+      ptrs[idx] = NULL;
+    }
+  }
+
+  for (int i = 0; i < num_ptrs; ++i) {
+    if (ptrs[i]) {
+      buddy_free(&pool, ptrs[i]);
+    }
+  }
+
+  check_buddy_pool_full(&pool);
+  buddy_destroy(&pool);
+}
+
+void test_fragmentation_and_reuse(void)
+{
+  fprintf(stderr, "->Testing fragmentation and reuse\n");
+  struct buddy_pool pool;
+  buddy_init(&pool, UINT64_C(1) << (SMALLEST_K + 4)); // Small pool
+
+  void *a = buddy_malloc(&pool, 32);
+  void *b = buddy_malloc(&pool, 64);
+  void *c = buddy_malloc(&pool, 32);
+  buddy_free(&pool, b); // Free middle
+  void *d = buddy_malloc(&pool, 64); // Should reuse b's space
+  TEST_ASSERT_EQUAL_PTR(b, d);
+  buddy_free(&pool, a);
+  buddy_free(&pool, c);
+  buddy_free(&pool, d);
+
+  check_buddy_pool_full(&pool);
+  buddy_destroy(&pool);
+}
+void test_randomized_realloc(void)
+{
+  fprintf(stderr, "->Testing randomized reallocs\n");
+  struct buddy_pool pool;
+  buddy_init(&pool, UINT64_C(1) << (SMALLEST_K + 6)); // 4KB pool
+
+  const int n = 64;
+  void *ptrs[n];
+  size_t sizes[n];
+  memset(ptrs, 0, sizeof(ptrs));
+  memset(sizes, 0, sizeof(sizes));
+
+  for (int i = 0; i < n; i++) {
+    sizes[i] = rand() % 64 + 1;
+    ptrs[i] = buddy_malloc(&pool, sizes[i]);
+    if (ptrs[i]) {
+      struct avail *block = ((struct avail *)ptrs[i]) - 1;
+      size_t block_size = (1UL << block->kval);
+      size_t usable_size = block_size - sizeof(struct avail);
+      memset(ptrs[i], i, usable_size);
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (!ptrs[i]) continue;
+    size_t new_size = (rand() % 64 + 1) * 2;
+    void *new_ptr = buddy_realloc(&pool, ptrs[i], new_size);
+    if (new_ptr) {
+      struct avail *block = ((struct avail *)new_ptr) - 1;
+      size_t block_size = (1UL << block->kval);
+      size_t usable_size = block_size - sizeof(struct avail);
+      memset(new_ptr, i, usable_size);
+      ptrs[i] = new_ptr;
+      sizes[i] = new_size;
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (ptrs[i]) buddy_free(&pool, ptrs[i]);
+  }
+
+  check_buddy_pool_full(&pool);
+  buddy_destroy(&pool);
+}
 
 int main(void) {
   time_t t;
@@ -145,5 +329,12 @@ int main(void) {
   RUN_TEST(test_buddy_init);
   RUN_TEST(test_buddy_malloc_one_byte);
   RUN_TEST(test_buddy_malloc_one_large);
-return UNITY_END();
+  RUN_TEST(test_btok);
+  RUN_TEST(test_multiple_allocs_and_frees);
+  RUN_TEST(test_null_and_zero_cases);
+  RUN_TEST(test_realloc_resize_up_and_down);
+  RUN_TEST(test_randomized_alloc_free);
+  RUN_TEST(test_fragmentation_and_reuse);
+  RUN_TEST(test_randomized_realloc);
+  UNITY_END();
 }
